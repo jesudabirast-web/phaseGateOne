13ea6674f0dd7d93f530c00443892b5c52965477
jesudabirast-web <jesudabira681@gmail.com>
2025-12-08 15:39:31 +0100

    second commit of the day

---
+++ Python/scambirah_app.py
@@ -0,0 +1,21 @@
+import random
+
+start_date = date.now()
+
+first_Number = random.randint*10
+second_Number = random.randint*10
+total = first_Number - second_Number
+user_input = (int(input("enter result of first number - second number:  "))
+
+total_Time = start_Time + end_Time / 60
+
+print(Problem_Solver(number))
+
+def ProblemSolver(number) 
+
+	if userAnswer == total:
+		print("correct answer"
+	
+	elif:			
+		print("incorrect answer")
+		
