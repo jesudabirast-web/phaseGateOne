aefa62c073df6fdcf3da1cf4c632b8073b5ef4be
jesudabirast-web <jesudabira681@gmail.com>
2025-12-08 15:37:35 +0100

    secondcommit

---
+++ Javascript/simpleArithmeticApp.js
@@ -0,0 +1,31 @@
+const prompt = require ('prompt-sync')();
+
+let start date = date.now()
+
+let firstNumber = (int(Math.random()*10);
+let secondNumber = (int(Math.random()*10);
+let total = firstNumber - secondNumber
+console.log("enter result of first number - second number:  ");
+let totalTime = startTime + endTime / 60
+
+console.log(ProblemSolver(number));
+
+public static int ProblemSolver(number) 
+
+	if(userAnswer == total)
+			console.log("correct answer");
+
+	}else{
+		(userAnswer != total)
+			console.log("incorrect answer")
+		
+
+
+
+
+
+let stop date = date.now()
+
+let result = (start date - stopdate) / -1000
+
+console.log("your total is" + total)
