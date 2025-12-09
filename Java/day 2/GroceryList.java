d21cee529b705a11075fad06e8b17e1bf8d84fc5
jesudabirast-web <jesudabira681@gmail.com>
2025-12-09 11:43:02 +0100

    first commit of today

---
+++ Java/day 2/GroceryList.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class GroceryList {
+	public static void main(String[]args){
+
+	Scanner input = new Scanner(System.in);
+	
+
+	System.out.println("1 - Add Items");
+	
+	System.out.println("2 - Remove Items");
+
+	System.out.println("3 - Show all available items");
+
+	System.out.print("Which one abeg:  ");
+	int userList = input.nextInt();
+
+	int grocery1 = bread;
+	int groc2 = yam;
+	int groc3 = flour;
+	int groc4 = beans;
+	int groc5 = eggs;
+	int gorc6 =butter;
+	int groc7 = potatoes;
+	
+
+	}
+
+System.out.print(Arrays.toString(GroceryStoreList(userList));
+
+
+	public static int GroceryStoreList(int number) {
+
+
+	if(userList ==  1){
+		System.out.print("   ");
+		System.out.print("    ");
+
+  }
+
+	else if(userList == 2){
+		System.out.print("available item(you can only add 2 each time):   ");
+		System.out.print("  bread   ");
+		System.out.print("   yam   ");	
+
+  }
+
+
+	else if(userList = 3){
+	System.out.print("which one you wan comot?:  ");
+	
+	removeItems.remove(number);
+
+ }
+
+return null;
+
+
+}
+
+
+
+}
