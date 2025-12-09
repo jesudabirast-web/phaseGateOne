98ac396ebe5963988100a3867f2f6f8588402a78
jesudabirast-web <jesudabira681@gmail.com>
2025-12-09 11:50:42 +0100

    today's first commit

--- Python/day 2/grocery_list.py
+++ Python/day 2/grocery_list.py
@@ -2,19 +2,18 @@ print("1 - Add items")
 print("2 - Remove items")
 print("3 - Show all available items")
 
-print("alaye pick one...please:  ")
+user_list = int(input("alaye pick one...please:  "))
 
-	if userList ==  1:
+if user_list ==  1:
 		print("   ")
 		print("    ")
   
 
-	else if userList == 2:
-		print"available item: "
+elif user_list == 2:
 		print("    ")
 		print("    ")
 
 
-	elif userList = 3:
+elif user_list == 3:
 		print("which one you wan comot?:  ");
 		removeItems.remove(number);
