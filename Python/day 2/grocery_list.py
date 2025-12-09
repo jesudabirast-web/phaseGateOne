ea8c8aab6182a7592443e36f2f45c555612079d5
jesudabirast-web <jesudabira681@gmail.com>
2025-12-09 11:44:21 +0100

    today's first commit

---
+++ Python/day 2/grocery_list.py
@@ -0,0 +1,20 @@
+print("1 - Add items")
+print("2 - Remove items")
+print("3 - Show all available items")
+
+print("alaye pick one...please:  ")
+
+	if userList ==  1:
+		print("   ")
+		print("    ")
+  
+
+	else if userList == 2:
+		print"available item: "
+		print("    ")
+		print("    ")
+
+
+	elif userList = 3:
+		print("which one you wan comot?:  ");
+		removeItems.remove(number);
